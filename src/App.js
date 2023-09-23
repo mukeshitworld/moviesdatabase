c32ee9852d1740e5c632316eb62aba7380566960
Mukesh Kumar Mishra <mukesh.itworld@gmail.com>
2023-09-23 18:07:11 +0530

    Updated API URL to use HTTPS
    Updated API URL to use HTTPS

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 //f17dd74c
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=f17dd74c';
+const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=f17dd74c';
 
 
 const App = () => {
